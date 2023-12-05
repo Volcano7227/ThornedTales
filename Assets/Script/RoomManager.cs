625a8e38c4be42835d6b473c16d183436cc0225b
JeremyDeschamps <Deschampsj007@gmail.com>
2023-12-05 10:55:20 -0500

    BossRoom Is Generating !

--- Assets/Script/RoomManager.cs
+++ Assets/Script/RoomManager.cs
@@ -4,11 +4,19 @@ using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UIElements;
+using System;
 
 //Inspired From : https://www.youtube.com/watch?v=eK2SlZxNjiU
+//To-Do 
+/*
+ * List Room (Randomize Layout)
+ * Boss Room
+  */
 public class RoomManager : MonoBehaviour
 {
-    [SerializeField] GameObject roomPrefab;
+    enum Direction { Up,Down,Left,Rigth }
+    [SerializeField] GameObject defaultRoomPrefab;
+    [SerializeField] GameObject bossRoomPrefab;
     [SerializeField] int maxRooms = 10;
     [SerializeField] int minRooms = 6;
     [SerializeField]int gridSizeX = 20;
@@ -22,9 +30,14 @@ public class RoomManager : MonoBehaviour
 
     Queue<Vector2Int> roomQueue = new();
 
+    // 0 : Vide , 1 : Room , 2 : BossRoom
     int[,] roomGrid;
+
     int roomCount;
     Vector2Int startingRoomIndex => new Vector2Int(gridSizeX / 2, gridSizeY / 2);
+
+    Room FirstRoom => Dungeon.transform.GetChild(0).GetComponent<Room>();
+    Room LastRoom => Dungeon.transform.GetChild(Dungeon.transform.childCount - 1).GetComponent<Room>();
 
     bool generationComplete = false;
     private void Awake()
@@ -47,10 +60,10 @@ public class RoomManager : MonoBehaviour
                 int gridX = roomIndex.x;
                 int gridY = roomIndex.y;
 
-                TryGenerateRoom(new Vector2Int(gridX - 1, gridY));
-                TryGenerateRoom(new Vector2Int(gridX + 1, gridY));
-                TryGenerateRoom(new Vector2Int(gridX, gridY - 1));
-                TryGenerateRoom(new Vector2Int(gridX, gridY + 1));
+                TryGenerateRoom(new Vector2Int(gridX - 1, gridY), defaultRoomPrefab);
+                TryGenerateRoom(new Vector2Int(gridX + 1, gridY), defaultRoomPrefab);
+                TryGenerateRoom(new Vector2Int(gridX, gridY - 1), defaultRoomPrefab);
+                TryGenerateRoom(new Vector2Int(gridX, gridY + 1), defaultRoomPrefab);
             }
             else if (roomCount < minRooms)
             {
@@ -59,14 +72,53 @@ public class RoomManager : MonoBehaviour
             }
             else
             {
+                GenerateBossRoom();
                 generationComplete = true;
             }
             yield return null;
         }
-        
-        Dungeon.transform.GetChild(Dungeon.transform.childCount - 1).TryGetComponent(out Room LastRoom);
-        LastRoom.PaintWall(Color.red);
+
         Debug.Log($"Generation completed - {roomCount} rooms generated");
+    }
+
+    Vector2Int GetGridIdFromDirection(Direction direction,Vector2Int posInit)
+    {
+        int gridX = posInit.x;
+        int gridY = posInit.y;
+        
+        switch (direction)
+        {
+            case Direction.Up:
+                return new Vector2Int(gridX, gridY + 1);
+            case Direction.Down:
+                return new Vector2Int(gridX, gridY - 1);
+            case Direction.Left:
+                return new Vector2Int(gridX - 1, gridY);
+            case Direction.Rigth:
+                return new Vector2Int(gridX + 1, gridY);
+            default:
+                throw new System.Exception("Wtf how/ You stupid");
+        }
+    }
+    void GenerateBossRoom()
+    {
+        bool generated = false;
+        Room currentRoom = LastRoom;
+        int currentRoomListId = Dungeon.transform.childCount - 1;
+        int directionI = 0;
+
+        while(!generated)
+        {
+            generated = TryGenerateRoom(GetGridIdFromDirection((Direction)directionI, currentRoom.RoomIndex), bossRoomPrefab, false, false, true, false);
+            
+            directionI++;
+            if(directionI == Enum.GetNames(typeof(Direction)).Length)
+            {
+                directionI = 0;
+                currentRoomListId -= 1;
+                Dungeon.transform.GetChild(currentRoomListId).TryGetComponent(out currentRoom);
+            }
+        } 
     }
     public bool TryGetRoomAt(Vector2Int id, out Room room)
     {
@@ -87,7 +139,7 @@ public class RoomManager : MonoBehaviour
         roomGrid[x, y] = 1;
         roomCount++;
 
-        var initialRoom = Instantiate(roomPrefab, GetPositionFromGridIndex(startingRoomId), Quaternion.identity, Dungeon.transform);
+        var initialRoom = Instantiate(defaultRoomPrefab, GetPositionFromGridIndex(startingRoomId), Quaternion.identity, Dungeon.transform);
 
         initialRoom.name = $"StartingRoom-{roomCount}";
         Room startingRoom = initialRoom.GetComponent<Room>();
@@ -96,35 +148,34 @@ public class RoomManager : MonoBehaviour
         startingRoom.PaintWall(Color.green);
         roomList.Add(initialRoom);
     }
-    bool TryGenerateRoom(Vector2Int roomId)
+    bool TryGenerateRoom(Vector2Int roomId, GameObject RoomTypePrefab, bool randomzied = true, bool sizeLimited = true, bool adjacentFiltered = true, bool adjacentToNoRoom = false)
     {
         int x = roomId.x;
         int y = roomId.y;
-
-        if(roomCount >= maxRooms)
+        int adjacentNb = adjacentToNoRoom ? 0 : 1;
+        if (roomCount >= maxRooms && sizeLimited)
             return false;
 
-        if (Random.value < 0.5f && roomId != Vector2Int.zero)
+        if (UnityEngine.Random.value < 0.5f && roomId != Vector2Int.zero && randomzied)
             return false;
         
         if (roomGrid[x, y] != 0)
             return false;
 
-        if (CountAdjacentRooms(roomId) > 1)
+        if (CountAdjacentRooms(roomId) > adjacentNb && adjacentFiltered)
             return false;
-
-
 
         roomQueue.Enqueue(roomId);
         roomGrid[x, y] = 1;
         roomCount++;
-
-        var newRoom = Instantiate(roomPrefab, GetPositionFromGridIndex(roomId), Quaternion.identity, Dungeon.transform);
+        
+        var newRoom = Instantiate(RoomTypePrefab, GetPositionFromGridIndex(roomId), Quaternion.identity, Dungeon.transform);
         newRoom.GetComponent<Room>().RoomIndex = roomId;
         newRoom.name = $"Room-{roomCount}";
         roomList.Add(newRoom);
 
         PlaceDoors(newRoom, x, y);
+        
 
         return true;
     }
@@ -191,6 +242,7 @@ public class RoomManager : MonoBehaviour
             BottomRoom.PlaceDoor(Vector2Int.up);
         }
     }
+
     /*private void OnDrawGizmos()
     {
         Gizmos.color = new Color(0,1,1,0.5f);

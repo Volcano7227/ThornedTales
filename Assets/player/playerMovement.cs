2c84e5504ddfad8291521b2d8b8f314329f504f0
JeremyDeschamps <Deschampsj007@gmail.com>
2023-12-08 14:27:43 -0500

    playermouvement terminer

--- Assets/player/playerMovement.cs
+++ Assets/player/playerMovement.cs
@@ -3,41 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed;
-    [SerializeField] List<Material> mats;
+    [SerializeField] List<Sprite> sprites;
 
     Rigidbody2D rb;
-    //MeshRenderer rd;
+    SpriteRenderer rd;
     Vector2 movementInput;
 
     // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        //rd = GetComponent<MeshRenderer>();
-        //rd.material = mats[0]; //starts down
+        rd = GetComponent<SpriteRenderer>();
+        rd.sprite = sprites[0]; //starts down
     }
     // Update is called once per frame
     void Update()
     {
-       /* if (Input.GetKeyDown(KeyCode.UpArrow))
-            rd.material = mats[1]; 
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            rd.sprite = sprites[1]; 
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
-            rd.material = mats[2];
+            rd.sprite = sprites[2];
         else if (Input.GetKeyDown(KeyCode.RightArrow))
-            rd.material = mats[3];
+            rd.sprite = sprites[3];
         else if (Input.GetKeyDown(KeyCode.DownArrow))
-            rd.material = mats[0];*/
+            rd.sprite = sprites[0];
     }
     private void FixedUpdate()
     {
         rb.velocity = movementInput;
     }
-    public void OnMove()
+    private void OnMove(InputValue inputValue)
     {
-        //movementInput = inputValue.Get<Vector2>();
+        movementInput = inputValue.Get<Vector2>();
         Debug.Log("Moving");
     }
 }

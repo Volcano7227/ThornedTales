193fa2e24175487b54ead38d9b8c3a75de9e3c52
JeremyDeschamps <Deschampsj007@gmail.com>
2023-12-08 13:39:32 -0500

    test yes yes

--- Assets/player/playerMovement.cs
+++ Assets/player/playerMovement.cs
@@ -2,16 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Windows;
 
 [RequireComponent(typeof(Rigidbody2D))]
 
-public class playerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] public float speed;
     [SerializeField] public List<Material> mats;
 
-    PlayerControls controls;
     Rigidbody2D rb;
     MeshRenderer rd;
     Vector2 moveInput;
@@ -20,34 +18,28 @@ public class playerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        controls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
         rd = GetComponent<MeshRenderer>();
         rd.material = mats[0]; //starts down
     }
-
-    private void OnEnable()
-    {
-       controls.Player_map.Enable();
-    }
-    private void OnDisable()
-    {
-        controls.Player_map.Disable();
-    }
-
     // Update is called once per frame
     void Update()
     {
-        moveInput = controls.Player_map.movement.ReadValue<Vector2>();
-        rb.velocity = moveInput * speed;
-
-        if (Keyboard.current.upArrowKey.IsPressed())
+        if (Input.GetKeyDown(KeyCode.UpArrow))
             rd.material = mats[1]; 
-        else if (Keyboard.current.leftArrowKey.IsPressed())
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
             rd.material = mats[2];
-        else if (Keyboard.current.rightArrowKey.IsPressed())
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
             rd.material = mats[3];
-        else if (Keyboard.current.downArrowKey.IsPressed())
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
             rd.material = mats[0];
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(1,0.5f);
+    }
+    private void OnMove(InputValue inputValue)
+    {
+        Debug.Log("Moving");
     }
 }

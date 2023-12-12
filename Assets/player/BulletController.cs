1650b4be1a47dc6fde99ac23edb098cdb5899988
Jean-Michel <jeanmishoo@gmail.com>
2023-12-12 17:32:53 -0500

    fixed bullet collision

--- Assets/player/BulletController.cs
+++ Assets/player/BulletController.cs
@@ -7,6 +7,7 @@ public class BulletController : MonoBehaviour
     public float lifeTime;
     public Animator animator;
     public Rigidbody2D rb;
+    BoxCollider2D collider;
 
     // Start is called before the first frame update
     void Start()
@@ -14,12 +15,19 @@ public class BulletController : MonoBehaviour
         StartCoroutine(DeathDelay());
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         rb.velocity = Vector3.zero;
+        collider.enabled = false;
         animator.SetTrigger("Explode");
+    }
+
+    public void DestroyBullet()
+    {
+        Destroy(gameObject);
     }
 
     IEnumerator DeathDelay()

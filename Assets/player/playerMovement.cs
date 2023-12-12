9f1567cfda934534b4fd41218d42c2a97d8f79ae
Jean-Michel <jeanmishoo@gmail.com>
2023-12-12 15:54:55 -0500

    player shoot

--- Assets/player/playerMovement.cs
+++ Assets/player/playerMovement.cs
@@ -13,6 +13,11 @@ public class PlayerMovement : MonoBehaviour
     Vector2 movementInput;
     public Animator animator;
 
+    public GameObject projectile;
+    public float bulletSpeed;
+    float lastFire;
+    public float fireDelay;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -24,6 +29,20 @@ public class PlayerMovement : MonoBehaviour
         // video: https://www.youtube.com/watch?v=whzomFgjT50
         animator.SetFloat("Horizontal", movementInput.x);
         animator.SetFloat("Vertical", movementInput.y);
+
+        float shootHor = Input.GetAxis("Shoot_Horizontal");
+        float shootVer = Input.GetAxis("Shoot_Vertical");
+        if (shootHor != 0 && Time.time > lastFire + fireDelay)
+        {
+            Shoot(shootHor, 0);
+            lastFire = Time.time;
+        }
+        else if (shootVer != 0 && Time.time > lastFire + fireDelay)
+        {
+            Shoot(0, shootVer);
+            lastFire = Time.time;
+        }
+
         animator.SetFloat("Speed", movementInput.sqrMagnitude);
     }
     private void FixedUpdate()
@@ -33,5 +52,21 @@ public class PlayerMovement : MonoBehaviour
     private void OnMove(InputValue inputValue)
     {
         movementInput = inputValue.Get<Vector2>();
+    }
+
+    void Shoot(float x, float y)
+    {
+        Vector3 direction = new Vector3(
+            (x < 0) ? Mathf.Floor(x) : Mathf.Ceil(x),
+            (y < 0) ? Mathf.Floor(y) : Mathf.Ceil(y),
+            0
+        );
+        GameObject bullet = Instantiate(projectile, transform.position, 
+            Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z + 90));
+        bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        Rigidbody2D bulletRB = bullet.GetComponent<Rigidbody2D>();
+        bulletRB.velocity = direction * bulletSpeed;
+        float angulo = Mathf.Atan2(bulletRB.velocity.y, bulletRB.velocity.x) * Mathf.Rad2Deg;
+        bullet.transform.rotation = Quaternion.AngleAxis(angulo, Vector3.forward);
     }
 }
